f12869dc2dc188bdf65d9464f1caf79734b93099
Kl0099 <lodhakrunal464@gmail.com>
2025-03-11 01:24:38 +0530

    added megiclink signup and login feature and also added forgotpassword feature

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -1,17 +1,17 @@
-export const addNewBlog = (req, res) => {};
+export const addNewBlog = (req, res) => { };
 
-export const getBlog = (req, res) => {};
+export const getBlog = (req, res) => { };
 
-export const getAllBlogs = (req, res) => {};
+export const getAllBlogs = (req, res) => { };
 
-export const removeBlog = (req, res) => {};
+export const removeBlog = (req, res) => { };
 
-export const updateBlog = (req, res) => {};
+export const updateBlog = (req, res) => { };
 
-export const likeBlog = (req, res) => {};
+export const likeBlog = (req, res) => { };
 
-export const unlikeBlog = (req, res) => {};
+export const unlikeBlog = (req, res) => { };
 
-export const commentToBlog = (req, res) => {};
+export const commentToBlog = (req, res) => { };
 
-export const removeComment = (req, res) => {};
+export const removeComment = (req, res) => { };
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes.js";
 import cropRouter from "./routes/crop.routes.js";
 import blogRouter from "./routes/blog.routes.js";
+import session from "express-session";
+import passport from "passport";
+import "./passportConfig.js"
 
 dotenv.config();
 
@@ -13,6 +16,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 app.use(cors());
 app.use(
   cors({
@@ -22,21 +26,26 @@ app.use(
   })
 );
 app.use(cookieParser());
+app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
+const connectionurl = 'mongodb://localhost:27017/kishanmitra'
 mongoose
-  .connect(process.env.MONGO_DB_URL)
+  .connect(connectionurl)
   .then(() => console.log("Connected to database"))
   .catch((error) => console.error(error));
 
-app.use("/api/users", userRouter);
+app.use("/api/auth", userRouter);
 app.use("/api/crops", cropRouter);
 app.use("/api/blogs", blogRouter);
 
 app.use((err, req, res, next) => {
+  console.log(err)
   const statusCode = err.statusCode || 500;
   const message = err.message || "Something went wrong";
 
@@ -46,3 +55,5 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+//hiiiiii
